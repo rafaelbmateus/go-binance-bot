ee21f5c9c12a9dea79e3984971a0389e2246645e
Rafael Mateus <rafaelmateus@hotmail.com.br>
2022-03-29 09:13:02 -0300

    refactor: logs and vars

--- bot/bot.go
+++ bot/bot.go
@@ -58,15 +58,13 @@ func (me *Bot) Watchdog(trade config.Trade) {
 
 // Monitor symbol to buy or sell.
 func (me *Bot) Monitor(trade config.Trade) error {
-	me.Log.Debug().Msgf("monitor %v", trade)
-
 	currentPrice, err := me.Binance.SymbolPrice(trade.GetSymbol(), trade.BuyWith(), "1m")
 	if err != nil {
 		return err
 	}
 
 	if currentPrice <= trade.BuyPrice {
-		me.Log.Debug().Msgf("time to buy %s with price %f buyPrice=%f",
+		me.Log.Debug().Msgf("[%s] current price=%.2f - nice to buy!",
 			trade.Symbol, currentPrice, trade.BuyPrice)
 		err := me.buy(trade, currentPrice)
 		if err != nil {
@@ -77,8 +75,8 @@ func (me *Bot) Monitor(trade config.Trade) error {
 	}
 
 	if currentPrice >= trade.SellPrice {
-		me.Log.Debug().Msgf("time to sell %s with price %f buyPrice=%f",
-			trade.Symbol, currentPrice, trade.BuyPrice)
+		me.Log.Debug().Msgf("[%s] current price=%.2f - nice to sell!",
+			trade.Symbol, currentPrice, trade.SellPrice)
 		err := me.sell(trade, currentPrice)
 		if err != nil {
 			return err
@@ -87,7 +85,7 @@ func (me *Bot) Monitor(trade config.Trade) error {
 		return nil
 	}
 
-	me.Log.Debug().Msgf("price of %s is %v buyPrice=%f sellPrice=%f",
+	me.Log.Debug().Msgf("[%s] current price=%.2f buy=%.2f sell=%.2f",
 		trade.Symbol, currentPrice, trade.BuyPrice, trade.SellPrice)
 	return nil
 }
@@ -114,7 +112,7 @@ func (me *Bot) buy(trade config.Trade, price float64) error {
 		return err
 	}
 
-	me.Log.Debug().Msgf("order to buy created %v", trade)
+	me.Log.Debug().Msgf("[%s] buy for %d", trade.Symbol, price)
 	me.Notify.SendMessage(notify.NewMessage(fmt.Sprintf("Order to buy created %v", order)))
 
 	return nil
@@ -137,12 +135,12 @@ func (me *Bot) sell(trade config.Trade, price float64) error {
 	}
 
 	order, err := me.Binance.CreateOrder(trade.GetSymbol(), trade.BuyWith(),
-		"SELL", wallet, trade.SellPrice)
+		"SELL", quantity, price)
 	if err != nil {
 		return err
 	}
 
-	me.Log.Debug().Msgf("order to sell created %v", trade)
+	me.Log.Debug().Msgf("[%s] sell for %d", trade.Symbol, price)
 	me.Notify.SendMessage(notify.NewMessage(fmt.Sprintf("Order to sell created %v", order)))
 
 	return nil
@@ -151,10 +149,10 @@ func (me *Bot) sell(trade config.Trade, price float64) error {
 func welcomeMessage(config config.Config) string {
 	msg := fmt.Sprintf("%s started! :money_mouth_face:\n\n", config.Name)
 	for i, trade := range config.Trades {
-		msg += fmt.Sprintf("> %d: %s\n", i+1, trade.Symbol)
-		msg += fmt.Sprintf("> Interval: %s\n", trade.Interval)
-		msg += fmt.Sprintf("> Purchase price: %f\n", trade.BuyPrice)
-		msg += fmt.Sprintf("> Sale Price: %f\n", trade.SellPrice)
+		msg += fmt.Sprintf("> **%d:** %s\n", i+1, trade.Symbol)
+		msg += fmt.Sprintf("> **Interval:** %s\n", trade.Interval)
+		msg += fmt.Sprintf("> **Purchase price:** %.2f\n", trade.BuyPrice)
+		msg += fmt.Sprintf("> **Sale Price:** %.2f\n", trade.SellPrice)
 	}
 
 	return msg
